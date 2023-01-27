0111ee697c6ae8131d47c344b2be8edd23afb445
Mustachedom <123716453+Mustachedom@users.noreply.github.com>
2023-01-26 21:24:16 -0800

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+name "mustache-stashes"
+author "mustache_dom"
+description "stashes by mustache dom"
+fx_version "cerulean"
+game "gta5"
+
+client_scripts {
+	'client.lua',
+	'target.lua',
+}
+
+lua54 'yes'
+
+dependency '/assetpacks'
