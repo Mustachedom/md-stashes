b70d0f0b400b1ca4f0e5ca1733949c927f765821
Mustachedom <123716453+Mustachedom@users.noreply.github.com>
2023-01-26 21:23:50 -0800

    Create target.lua

---
+++ target.lua
@@ -0,0 +1,25 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+CreateThread(function()
+exports['qb-target']:AddBoxZone("stachename1",vector3location, 1.5, 1.75, { -- 963.37, -2122.95, 31.47
+	name = "stachename1",
+	heading = 186.0,
+	debugPoly = true,
+	minZ = 37.77834,
+	maxZ = 41.87834,
+}, {
+	options = {
+		{
+            type = "client",
+            event = "md-stashes:client:AutoCare",
+			icon = "fas fa-sign-in-alt",
+			label = "open stash",
+			--job = "mechanic",
+			--gang = "ballas",
+			--item = "metalscrap"
+		},
+	},
+	distance = 2.5
+ })
+
+end)
