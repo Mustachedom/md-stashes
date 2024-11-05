4ddc3d0a87db0ae61fbdd0cea603c9e9ded347fe
Mustache Dom <123716453+Mustachedom@users.noreply.github.com>
2024-11-04 18:40:52 -0800

    check

--- shared/Config.lua
+++ shared/Config.lua
@@ -23,6 +23,7 @@ Config.Objects = {
     loc = vector3
 
 ]]
+-- seriously this table below is not needed anymore just for backwards compatability and wont work with the new system
 Config.stash = { ------ outdated dont use if you are downloading new, this is meant for previous users that can use a command to inject this data to the SQL
     ['test'] = { loc = vector3(520.29736328125, -3108.0920410156, 33.285308837891), password = 1234},
     ['test2'] = { loc = vector3(2746.3481445312, 2787.6713867188, -35.787368774414), item = 'lockpick', slots = 12, weight = 10000},
