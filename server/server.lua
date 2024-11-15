eb6d3fac86b4d5c4a7a833e5e0388e09cff886d5
Mustache Dom <123716453+Mustachedom@users.noreply.github.com>
2024-11-14 18:56:12 -0800

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -47,18 +47,6 @@ if logs == false then return end
      end)
 end
 
-CreateThread(function()
-if Config.Inv == 'ox' then
-    for k, v in pairs (Config.stash) do 
-        if v.weight == nil then v.weight = Config.Defaultweight end
-	    if v.slots == nil then v.slots = Config.Defaultslot end
-        local stash = { id = k, label = v.name, slots = v.slots, weight = v.weight, }
-        exports.ox_inventory:RegisterStash(stash.id, stash.label, stash.slots, stash.weight)
-    end
-end 
-
-end)
-
 lib.addCommand('newstash', {
     help = "Make A New Stash",
     restricted = 'group.admin',
