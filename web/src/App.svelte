a8b26dce7b70a85744585db1a8697f7401ccefb0
MadCap <95227673+ThatMadCap@users.noreply.github.com>
2025-09-27 11:52:57 +0100

    refactor: EditStash styles to global CSS
    - swapped component-specific styles from EditStash.svelte to global.css file for future reusability
    - updated class names to match new global styles

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -2,6 +2,7 @@
   import VisibilityProvider from './providers/VisibilityProvider.svelte';
   import CreateStash from './components/CreateStash.svelte';
   import { initDevEnvironment } from './utils/mockData';
+  import './styles/global.css';
 
   initDevEnvironment();
 </script>
